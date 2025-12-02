8269e7e0620a17afea5d1fef5c226c538f5f799c
iosky <2061469002@qq.com>
2025-12-02 14:47:06 +0800

    项目初始化

---
+++ src/routes/_authed/dashboard/-components/dashboard-sidebar.tsx
@@ -0,0 +1,41 @@
+import { Command } from 'lucide-react'
+import type * as React from 'react'
+import {
+	Sidebar,
+	SidebarContent,
+	SidebarFooter,
+	SidebarHeader,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+} from '@/components/ui/sidebar'
+
+export function DashboardSidebar({
+	...props
+}): React.ComponentProps<typeof Sidebar> {
+	return (
+		<Sidebar collapsible="icon" {...props}>
+			<SidebarHeader>
+				<SidebarMenu>
+					<SidebarMenuItem>
+						<SidebarMenuButton size="lg" asChild>
+							<a href="#">
+								<div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+									<Command className="size-4" />
+								</div>
+								<div className="grid flex-1 text-left text-sm leading-tight">
+									<span className="truncate font-medium">Presto</span>
+									<span className="truncate text-xs">
+										attention is all you need.
+									</span>
+								</div>
+							</a>
+						</SidebarMenuButton>
+					</SidebarMenuItem>
+				</SidebarMenu>
+			</SidebarHeader>
+			<SidebarContent></SidebarContent>
+			<SidebarFooter></SidebarFooter>
+		</Sidebar>
+	)
+}
